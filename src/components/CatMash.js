72474b2b87ef7be08e35e27cc1b86ee7653e666c
Karolina Gauvain <karolina.gauvain@gmail.com>
2018-10-22 23:52:44 +0200

    Add cats' photos from cats.json

--- src/components/CatMash.js
+++ src/components/CatMash.js
@@ -1,31 +1,36 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
-import logo from './logo.svg';
+import cats from './cats.json';
 
 class CatMash extends Component {
   state = {
-    cats: []
   }
 
   render() {
+    const catImg = cats.images.map(cat => {return <img src ={cat.url} alt="cat" className="cat-image"/>;});
+    //Display random cats
+    const cat1 = catImg[Math.floor(Math.random() * cats.images.length)];
+    const cat2 = catImg[Math.floor(Math.random() * cats.images.length)];
+
     return (
       <div>
         <header className="cat-mash-title">
           <h1>Cat Mash</h1>
+          <h4>Which cat is cuter?</h4>
         </header>
         <main className="cat-mash-content">
           <div className="cat-photos">
-            <img src={logo} className="App-logo" alt="logo" />
-            <img src={logo} className="App-logo" alt="logo" />
-
+            {cat1}
+            <p>OR</p>
+            {cat2}
           </div>
-
         </main>
         <footer>
           <div className="open-results">
               <Link
                 to='/results'
-                >See Results</Link>
+                >See the most beautiful cats</Link>
+                <p>Number of votes</p>
           </div>
         </footer>
       </div>
